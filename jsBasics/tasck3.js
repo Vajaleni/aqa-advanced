1c801577ecfc163a6711966165a15b473f047405
vajaleni <lena37376@gmail.com>
2024-08-08 10:31:54 +0300

    Add homework 4 files

---
+++ jsBasics/tasck3.js
@@ -0,0 +1,13 @@
+{
+    let age = 45;
+let isAdult = age >= 18;
+console.log ('Вік персони: ' + age);
+console.log('Чи особа повнолітня?: ' + isAdult);
+}
+
+{
+let age1 = 17;
+let isAdult1 = age1 >= 18;
+console.log ('Вік персони: ' + age1);
+console.log('Чи особа повнолітня?: ' + isAdult1)
+}
---
+++ jsBasics/task1.js
@@ -0,0 +1,11 @@
+let myString = 'I learn English';
+const myNumber = 35;
+const myBoolean = true;
+let myNull = null;
+let myUnderfined
+
+console.log (myString);
+console.log (myNumber);
+console.log (myBoolean);
+console.log (myNull);
+console.log (myUnderfined);
