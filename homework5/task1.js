cefd531749c08f128438ab5004382c90c2ab4dbe
vajaleni <lena37376@gmail.com>
2024-08-11 12:41:56 +0300

    Add homework5

---
+++ homework5/task1.js
@@ -0,0 +1,17 @@
+let grade = 100;
+
+if (grade<60) {
+    console.log('Незадовільно')
+} 
+else if (grade<=70) {
+    console.log('Задовільно')
+} 
+else if (grade<=80) {
+    console.log('Добре')
+}
+else if (grade<=90) {
+    console.log('Дуже добре')
+} 
+else if (grade<=100) {
+    console.log('Відмінно')
+}
