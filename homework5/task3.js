978678c8479b1e5b0f8e862a477326b2e956fdd5
vajaleni <lena37376@gmail.com>
2024-08-14 09:41:07 +0300

    Add folder functional

---
+++ homework5/task3.js
@@ -0,0 +1,14 @@
+
+const b=5
+for (let i=1; i<=10;i++) {
+    console.log(`${b}*${i} = ${b*i}`);
+    }
+
+console.log('.....')
+
+const c=5;
+let i=1;
+while (i<=10) {
+    console.log(`${c}*${i} = ${c*i}`);
+    i++;
+}
