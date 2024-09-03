9a141d8e53407aa536faa04e2e5feee40e0c608c
vajaleni <lena37376@gmail.com>
2024-09-03 11:06:13 +0300

    Add tasks

---
+++ homework8/task6.js
@@ -0,0 +1,6 @@
+let  numbersList = [1,10,14,2,4,5,43,34];
+const sortedList = [...numbersList];
+sortedList.sort ((a,b)=> a - b);
+
+console.log ('Оригінальний масив: ', numbersList);
+console.log ('Відсортований масив: ', sortedList)
